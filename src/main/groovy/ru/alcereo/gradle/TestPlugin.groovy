442347d6c37a13cb11b89ab71ab6d7ab6bd5e920
alcereo <ak-91@mail.ru>
2017-12-22 01:39:39 +0300

    Test some bash Add SNAPSHOT split Gradle task group and description some tests

--- src/main/groovy/ru/alcereo/gradle/TestPlugin.groovy
+++ src/main/groovy/ru/alcereo/gradle/TestPlugin.groovy
@@ -14,12 +14,43 @@ class TestPlugin implements Plugin<Project>{
         project.configure(project){
 
             apply plugin: 'java'
+
+            sourceCompatibility = 1.7
+
+            ext {
+                springBootConfig = 'org.springframework.boot:spring-boot-starter-test:2.0.0.M7'
+            }
+
+            repositories{
+                mavenCentral()
+                maven{
+                    url 'http://repo.spring.io/milestone/'
+                }
+            }
         }
 
+        def ALCEREO_GROUP = 'alcereo-tasks'
+
         project.task("alc-test"){
+            group = ALCEREO_GROUP
+            description = "Вывод тестовой строки.. Те се.."
 
-            println "real test 3...."
+            doLast{
 
+                println "real test 3...."
+            }
+        }
+
+
+        project.task("shell-script-test"){
+            group = ALCEREO_GROUP
+            description = "Выполнение скрипта..."
+
+            doLast{
+
+                def proc = "git --version".execute()
+                proc.waitForProcessOutput(System.out, System.err)
+            }
         }
 
     }
